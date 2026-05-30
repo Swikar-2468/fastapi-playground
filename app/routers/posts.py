093fd601611eff52f26c2deb1f94d5e0de83c161
Swikar Basnet <swikarbasnet@gmail.com>
2026-05-30 17:04:24 +0545

    optimized url with query parameters

--- app/routers/posts.py
+++ app/routers/posts.py
@@ -17,12 +17,12 @@ router = APIRouter(
 
 
 @router.get("/", response_model=List[schemas.Post])
-def get_posts(db: Session = Depends(get_db), current_user:int = Depends(oauth2.get_current_user)):
+def get_posts(db: Session = Depends(get_db), current_user:int = Depends(oauth2.get_current_user), limit:int = 10, skip:int = 0, search:Optional[str] = ""):
     # to view all posts
-    # posts = db.query(models.Post).all()
+    posts = db.query(models.Post).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
 
     # if we want the user to be able to see only their posts then 
-    posts = db.query(models.Post).filter(models.Post.owner_id == current_user.id).all()
+    # posts = db.query(models.Post).filter(models.Post.owner_id == current_user.id).all()
     return posts
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
@@ -44,6 +44,9 @@ def get_posts(id:int, db:Session=Depends(get_db), current_user:int = Depends(oau
 
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id:{id} was not found!!!")
+
+    # if post.owner_id != current_user.id:
+    #     raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Post deletion not Authorized")
 
     return post
 
--- app/schemas.py
+++ app/schemas.py
@@ -2,6 +2,20 @@ from pydantic import BaseModel, EmailStr
 from datetime import datetime
 from typing import Optional
 
+class UserCreate(BaseModel):
+    email : EmailStr
+    password : str    
+
+class UserOut(BaseModel):
+    id:int
+    email:EmailStr
+    created_at:datetime
+
+class UserLogin(BaseModel):
+    email:EmailStr
+    password : str
+
+
 class PostBase(BaseModel):
     title:str
     content:str
@@ -15,19 +29,7 @@ class Post(PostBase):
     id:int
     created_at:datetime
     owner_id:int
-
-class UserCreate(BaseModel):
-    email : EmailStr
-    password : str    
-
-class UserOut(BaseModel):
-    id:int
-    email:EmailStr
-    created_at:datetime
-
-class UserLogin(BaseModel):
-    email:EmailStr
-    password : str
+    owner : UserOut
 
 class Token(BaseModel):
     access_token:str
