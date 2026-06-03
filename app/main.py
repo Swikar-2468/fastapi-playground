e9812ea0551e797c7ce6e3f447be6773ab79aa51
Swikar Basnet <swikarbasnet@gmail.com>
2026-06-03 21:00:47 +0545

    added voting functionality

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from .routers import posts, users, auth
+from .routers import posts, users, auth, votes
 from .config import settings
 
 app = FastAPI()
@@ -9,6 +9,7 @@ print(settings)
 app.include_router(posts.router)
 app.include_router(users.router)
 app.include_router(auth.router)
+app.include_router(votes.router)
 
 @app.get("/")    
 def root():
