c33c0133d0eb986c9a1fffe1c2dd08a9ee9e3ce4
Swikar Basnet <swikarbasnet@gmail.com>
2026-06-10 22:00:24 +0545

    CORS setup

--- app/main.py
+++ app/main.py
@@ -1,8 +1,25 @@
 from fastapi import FastAPI
 from .routers import posts, users, auth, votes
 from .config import settings
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+# origins = [
+#     "http://localhost",
+#     "http://localhost:8080",
+#     "https://www.google.com",
+#     "http://localhost:3000"
+# ]
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"], #allow all methods
+    allow_headers=["*"],
+)
 
 print(settings)
 
